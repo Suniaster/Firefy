6cd2b601a0b70b3cb86402ab82d0417c882fbc81
Suniaster <thibousbr@gmail.com>
2020-04-07 16:50:40 -0300

    bugfix production

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(3000);
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log('Server alive')
+});
