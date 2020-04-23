35f2705529a79a027c365d5b647d3acccb66acaf
Suniaster <thibousbr@gmail.com>
2020-04-23 06:48:56 -0300

    Added ssas to project

--- src/pages/RoomsIndex.js
+++ src/pages/RoomsIndex.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react'
 import api from '../services/api'
-import { Button } from 'react-bootstrap'
+import { Button, Modal , Form} from 'react-bootstrap'
 import { withRouter } from "react-router-dom";
 
 import RoomList from '../components/RoomList';
 import Header from '../components/Header'
+
+import '../styles/Modal.scss'
 class RoomIndex extends Component{
 
     constructor(props){
         super(props)
         this.state = {
             rooms: [],
-            inputRoom: ''
+            inputRoom: '',
+            modals:{
+                createRoom: false
+            },
+            inputs:{
+                roomName: ''
+            }
         }
     }
 
@@ -31,7 +39,10 @@ class RoomIndex extends Component{
         }).catch((err)=>{
             alert("Choose other name for your room")
         })
+    }
 
+    changeModal = (modalName, newState) =>{
+        this.setState({modals: {[modalName]: newState}})
     }
     render = () =>{
         
@@ -44,16 +55,8 @@ class RoomIndex extends Component{
             <Header>
             <div style={{height: "1px", backgroundColor: "rgb(10, 11, 24)"}}></div>
             <div className="main-container">
-                <div>
-                    <input 
-                        className="chat-input"
-                        type="text" 
-                        placeholder="Nome da sala" 
-                        value={this.state.inputRoom} 
-                        onChange={(e)=>this.setState({inputRoom: e.target.value})}
-                    />
-                </div>
-                <Button onClick={this.createRoom}>Criar sala</Button>
+            
+                <Button onClick={()=>this.changeModal('createRoom', true)}>Criar sala</Button>
 
                 <div className="rooms-container">
                     <h1 style={{textAlign:"center", marginTop: "30px"}}> Salas: </h1>
@@ -61,6 +64,41 @@ class RoomIndex extends Component{
                 </div>
                
             </div>
+
+            <Modal 
+                show={this.state.modals.createRoom} 
+                onHide={()=>this.changeModal('createRoom', false)} 
+                animation
+                centered
+            >
+                <Modal.Header className="room-modal-header">
+                    <Modal.Title>Create new Room</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="room-modal-background">
+                    <div>  
+                        <input 
+                            className="chat-input"
+                            type="text" 
+                            placeholder="Room name (empty for random)" 
+                            value={this.state.inputs.roomName} 
+                            onChange={(e)=>this.setState({inputs: {roomName: e.target.value}})}
+                        />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer className="room-modal-background">
+                    <Button 
+                    variant="secondary" 
+                    onClick={()=>{
+                        this.createRoom()
+                        this.dimissModal('createRoom', false)
+                    }}
+                    >
+                    Confirm
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+
             </Header>
         )
     }
