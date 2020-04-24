80019b013cb3c04881c5de9bc1d681cc4c7ee293
Suniaster <thibousbr@gmail.com>
2020-04-24 02:43:10 -0300

    Create modal to enter password for room

--- src/components/RoomIndex/ListRooms.js
+++ src/components/RoomIndex/ListRooms.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react'
 
-import RoomList from './RoomList';
 import api from '../../services/api';
-import '../../styles/rooms.css'
-import { Link } from 'react-router-dom'
+import '../../styles/rooms.scss'
+import { Link, withRouter } from 'react-router-dom'
+import {Modal, Form, Button} from 'react-bootstrap'
 
+import { FaUser, FaLock } from 'react-icons/fa'
 
-import { FaHome, FaGlobeAmericas, FaUser, FaSignOutAlt } from 'react-icons/fa'
-
-export default class ListRooms extends Component{
+class ListRooms extends Component{
 
     state = {
-        rooms: []
+        rooms: [],
+        modalOpen: false,
+        passwordInput: ''
     }
+
+    selectedId = ''
 
     componentDidMount(){
         api.get('/rooms').then((res)=>{
@@ -22,33 +25,92 @@ export default class ListRooms extends Component{
         })
     }
 
+    _handleRoomClick(roomId, isPrivate){
+        if(isPrivate){
+            this.selectedId = roomId
+            this.setState({modalOpen: true})
+        }
+        else{
+            this.props.history.push("/room/"+roomId)
+        }
+    }
+
+    _handlePasswordSubmit = () =>{
+        let roomId = this.selectedId
+        localStorage.setItem("@room-lastpassword", this.state.passwordInput)
+        this.props.history.push("/room/"+roomId)
+    }
+
     render(){
         const rooms = this.state.rooms.map((props)=>{
-            let roomId = props.roomId
-            let user_count =  props.userCount || 0
-            let max_capacity =  props.maxCapacity || 99
+            const {roomId, userCount, maxCapacity, host, isPrivate} = props
+            console.log(props)
             return (
-                <div className="room-list">
+                <button className="room-list" 
+                    onClick={()=>this._handleRoomClick(roomId, isPrivate)}>
                     <div className="icon-container">
                         <div className="icon">
-                            <faUser />
+                            <FaUser />
                         </div>
+                        {isPrivate && (
+                            <div className="icon">
+                                <FaLock />
+                            </div>
+                        )}
                         <div>
-                            {user_count}/{max_capacity}
+                            {userCount}/{maxCapacity}
                         </div>
                     </div>
                     <div>
-                    <Link to={"/room/"+roomId}>{roomId}</Link>
+                        <div>{roomId}</div>
+                        <div className="host-text">Host: {host}</div>
                     </div>
-                </div>
+                </button>
+    
             )
         })
         return (
             <div className="rooms-container">
                 <h1 style={{textAlign:"center", marginTop: "30px"}}> Salas: </h1>
                 {rooms}
+            
+            <Modal 
+                show={this.state.modalOpen} 
+                onHide={()=>this.setState({modalOpen:false})} 
+                animation
+                centered
+                >
+                <Modal.Header className="room-modal-header">
+                    <Modal.Title>Create new Room</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="room-modal-background">
+                    <div>  
+                        <Form>
+                            <Form.Group controlId="formInput">
+                                <Form.Label>Room require password</Form.Label>
+                                <Form.Control 
+                                    value={this.state.passwordInput}
+                                    onChange={(evt)=>this.setState({passwordInput: evt.target.value})}
+                                    type="text" 
+                                    placeholder="Room name (empty for random)" 
+                                    className="firefy-form-input"
+                                />
+                            </Form.Group>
+                        </Form>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer className="room-modal-background">
+                    <Button 
+                    variant="secondary" 
+                    onClick={this._handlePasswordSubmit}
+                    >
+                    Confirm
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             </div>
-
         )
     }
 }
+
+export default withRouter(ListRooms)
