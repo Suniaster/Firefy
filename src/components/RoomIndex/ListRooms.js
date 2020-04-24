ec8d7cbf728e58b9f935f64a8021f33e0bd447c5
Suniaster <thibousbr@gmail.com>
2020-04-24 01:58:28 -0300

    Deletes RoomList component
    Redundant name and uncessary code

--- src/components/RoomIndex/ListRooms.js
+++ src/components/RoomIndex/ListRooms.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react'
 
 import RoomList from './RoomList';
 import api from '../../services/api';
+import '../../styles/rooms.css'
+import { Link } from 'react-router-dom'
 
+
+import { FaHome, FaGlobeAmericas, FaUser, FaSignOutAlt } from 'react-icons/fa'
 
 export default class ListRooms extends Component{
 
@@ -13,14 +17,30 @@ export default class ListRooms extends Component{
     componentDidMount(){
         api.get('/rooms').then((res)=>{
             let rooms = res.data
+            console.log(rooms)
             this.setState({rooms: rooms})
         })
     }
 
     render(){
         const rooms = this.state.rooms.map((props)=>{
+            let roomId = props.roomId
+            let user_count =  props.userCount || 0
+            let max_capacity =  props.maxCapacity || 99
             return (
-                <RoomList {...props} />
+                <div className="room-list">
+                    <div className="icon-container">
+                        <div className="icon">
+                            <faUser />
+                        </div>
+                        <div>
+                            {user_count}/{max_capacity}
+                        </div>
+                    </div>
+                    <div>
+                    <Link to={"/room/"+roomId}>{roomId}</Link>
+                    </div>
+                </div>
             )
         })
         return (
